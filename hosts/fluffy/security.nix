d6534121947eb5915741d13b706c4e61e3ef624a
yxqsnz <yxqsnz@gmail.com>
2022-11-15 12:04:24 -0300

    Security: enable polkit

--- hosts/fluffy/security.nix
+++ hosts/fluffy/security.nix
@@ -2,6 +2,7 @@
   security = {
     sudo.enable = false;
     rtkit.enable = true;
+    polkit.enable = true;
     doas = {
       enable = true;
       extraRules = [{ groups = [ "wheel" ]; keepEnv = true; persist = true; }];
